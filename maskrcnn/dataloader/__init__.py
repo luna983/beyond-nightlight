f7f2f9d3c870d1b12a63abcff775197f31d9f04c
Yue Huang <yue.huang.luna@gmail.com>
2019-06-26 15:26:03 -0700

    has_target undefined error

--- maskrcnn/dataloader/__init__.py
+++ maskrcnn/dataloader/__init__.py
@@ -32,7 +32,7 @@ class MaskRCNNBatch(object):
     def pin_memory(self):
         for tensor in self.image:
             tensor = tensor.pin_memory()
-        if has_target:
+        if hasattr(self, target):
             for item in self.target:
                 item['masks'] = item['masks'].pin_memory()
                 item['labels'] = item['labels'].pin_memory()
