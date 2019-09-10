7f4120bb2d8d1fcfbc8c14b797ef4b5ad4c72e4e
Yue Huang <yue.huang.luna@gmail.com>
2019-09-10 11:33:21 -0700

    update preprocess

--- utils/preprocess_openaitanzania.py
+++ utils/preprocess_openaitanzania.py
@@ -140,10 +140,10 @@ if __name__ == '__main__':
     file_ids = [os.path.basename(f).split('.')[0]
                 for f in glob(os.path.join(IN_ANN_DIR, '*.geojson'))]
     # parameters
-    CHIP_SIZE = 800  # pixels
-    DOWN_RESOLUTION_FACTOR = 1  # resolution = this x 7.7cm
-    WINDOW_SIZE = CHIP_SIZE * DOWN_RESOLUTION_FACTOR
-    SAMPLE_RATIO = 1
+    CHIP_SIZE = 1600  # pixels
+    DOWN_RESOLUTION_FACTOR = 1.2  # resolution = this x 7.7cm
+    WINDOW_SIZE = int(CHIP_SIZE * DOWN_RESOLUTION_FACTOR)
+    SAMPLE_RATIO = 1.2
     # process every file
     for file_id in tqdm(file_ids):
         process_file(file_id)
