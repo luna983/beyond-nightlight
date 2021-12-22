4433bc268da68e380856134f4ac7dc33143a925b
luna983 <yue.huang.luna@gmail.com>
2021-12-22 19:27:41 +0000

    uncomment

--- scripts/gd_fig_ate.R
+++ scripts/gd_fig_ate.R
@@ -252,20 +252,20 @@ main <- function(input_file, output_figure_file, output_raw_data_file) {
 }
 
 # Creates the main figure
-# main(input_file = 'data/Siaya/Merged/main_res0.0010.csv',
-#      output_figure_file = 'output/fig-ate/fig-ate-raw.pdf',
-#      output_raw_data_file = 'fig_raw_data/fig-ate.csv')
+main(input_file = 'data/Siaya/Merged/main_res0.0010.csv',
+     output_figure_file = 'output/fig-ate/fig-ate-raw.pdf',
+     output_raw_data_file = 'fig_raw_data/fig-ate.csv')
 
 # Sensitivity checks
-# main(input_file = 'data/Siaya/Merged/main_res0.0005.csv',
-#      output_figure_file = 'output/fig-ate/fig-ate-res-sensitivity-0.0005-raw.pdf',
-#      output_raw_data_file = 'output/fig-ate/archive_data/fig-ate-res-sensitivity-0.0005.csv')
-# main(input_file = 'data/Siaya/Merged/main_res0.0015.csv',
-#      output_figure_file = 'output/fig-ate/fig-ate-res-sensitivity-0.0015-raw.pdf',
-#      output_raw_data_file = 'output/fig-ate/archive_data/fig-ate-res-sensitivity-0.0015.csv')
-# main(input_file = 'data/Siaya/Merged/main_res0.0020.csv',
-#      output_figure_file = 'output/fig-ate/fig-ate-res-sensitivity-0.0020-raw.pdf',
-#      output_raw_data_file = 'output/fig-ate/archive_data/fig-ate-res-sensitivity-0.0020.csv')
+main(input_file = 'data/Siaya/Merged/main_res0.0005.csv',
+     output_figure_file = 'output/fig-ate/fig-ate-res-sensitivity-0.0005-raw.pdf',
+     output_raw_data_file = 'output/fig-ate/archive_data/fig-ate-res-sensitivity-0.0005.csv')
+main(input_file = 'data/Siaya/Merged/main_res0.0015.csv',
+     output_figure_file = 'output/fig-ate/fig-ate-res-sensitivity-0.0015-raw.pdf',
+     output_raw_data_file = 'output/fig-ate/archive_data/fig-ate-res-sensitivity-0.0015.csv')
+main(input_file = 'data/Siaya/Merged/main_res0.0020.csv',
+     output_figure_file = 'output/fig-ate/fig-ate-res-sensitivity-0.0020-raw.pdf',
+     output_raw_data_file = 'output/fig-ate/archive_data/fig-ate-res-sensitivity-0.0020.csv')
 main(input_file = 'data/Siaya/Merged/main_res0.0025.csv',
      output_figure_file = 'output/fig-ate/fig-ate-res-sensitivity-0.0025-raw.pdf',
      output_raw_data_file = 'output/fig-ate/archive_data/fig-ate-res-sensitivity-0.0025.csv')
